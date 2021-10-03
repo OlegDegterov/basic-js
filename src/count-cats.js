7f90eca9d2a49ecb646e44fb0dbd28c59f1e181c
degterov_o@garpix.com <degterov_o@garpix.com>
2021-10-03 17:34:04 +0300

    feat: add count-cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,7 +14,15 @@ import { NotImplementedError } from '../extensions/index.js';
  * ]) => 3`
  *
  */
-export default function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
+export default function countCats(matrix ) {
+  //throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  let array = matrix.flat();
+  let count = 0;
+  for (let i = 0; i<array.length; i++){
+    if (array[i]==="^^") {
+      count+=1
+    }
+  }
+  return count
 }
