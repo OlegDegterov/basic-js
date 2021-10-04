4ea5aaa91b9b6a568c92dbe75208044add20d103
degterov_o@garpix.com <degterov_o@garpix.com>
2021-10-04 11:40:48 +0300

    feat: add whats-season

--- src/what-season.js
+++ src/what-season.js
@@ -11,7 +11,20 @@ import { NotImplementedError } from '../extensions/index.js';
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-export default function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function getSeason(date ) {
+  if (!date) {
+    throw new Error ('Unable to determine the time of year!')
+  }
+  if (!date.getMonth() && date.getMonth() !== 0) {
+    throw new Error ('Invalid date!')
+  }
+  if ((date.getMonth()) === 2 || (date.getMonth()) === 3 || (date.getMonth()) === 4)
+  {return 'spring'}
+  if ((date.getMonth()) === 5 || (date.getMonth()) === 6 || (date.getMonth()) === 7)
+  {return 'summer'}
+  if ((date.getMonth()) === 8 || (date.getMonth()) === 9 || (date.getMonth()) === 10)
+  {return 'autumn'}
+  if ((date.getMonth()) === 0 || (date.getMonth()) === 1 || (date.getMonth()) === 11)
+  {return 'winter'}
+  return false
 }
