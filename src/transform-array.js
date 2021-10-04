6dde38f0882579aff30fc4d15fc9fdb5fe11a21f
degterov_o@garpix.com <degterov_o@garpix.com>
2021-10-04 15:46:21 +0300

    fix: transform-array_fix(only 2 bug)

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,7 +13,33 @@ import { NotImplementedError } from '../extensions/index.js';
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-export default function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function transform(arr) {
+  let newArray = arguments[0].map(x=>x);
+  if (!Array.isArray(arr)) {
+    throw new Error ('\'arr\'parameter must be an instance of the Array!')
+  }
+  for (let i = 0; i< newArray.length; i++) {
+    if (newArray[i] === '--discard-next') {
+      newArray.splice(i,2)
+    }
+    if (newArray[i] === '--discard-prev') {
+      if ((i-1)<0){newArray.splice(i,1)}
+      else {newArray.splice(i-1,2)}
+    }
+    if (newArray[i] === '--double-next') {
+      if ((newArray[i+1])===undefined) {newArray.splice(i,1)}
+      else {
+        newArray.splice(i,1)
+        newArray.splice(i, 0, newArray[i]);
+      }
+    }
+    if (newArray[i] === '--double-prev') {
+      if ((newArray[i-1])===undefined) {newArray.splice(i,1)}
+      else {
+        newArray.splice(i,1)
+        newArray.splice(i, 0, newArray[i-1]);
+      }
+    }
+  }
+  return newArray
 }
